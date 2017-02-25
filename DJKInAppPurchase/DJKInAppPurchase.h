e70c9dec73a34f44ef6c3d702a2c165c67fe3a85
WataruSuzuki <wataru0406@gmail.com>
2017-02-25 13:30:00 +0900

    Change build settings about header files

--- DJKInAppPurchase/DJKInAppPurchase.h
+++ DJKInAppPurchase/DJKInAppPurchase.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char DJKInAppPurchaseVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DJKInAppPurchase/PublicHeader.h>
 
-//#import <DJKInAppPurchase/DJKInAppPurchaseImpl.h>
+#import <DJKInAppPurchase/DJKInAppPurchaseImpl.h>
+#import <DJKInAppPurchase/DJKKeychainManager.h>
